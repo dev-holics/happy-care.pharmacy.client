d9992f7264bd8ab0cf5a36513cc447e42ed1b3e7
Trương Minh Đức <58778364+derektruong@users.noreply.github.com>
2022-11-10 01:26:46 +0700

    feat: create order page (#11)

--- src/app/pages/cart/cart-routing.module.ts
+++ src/app/pages/cart/cart-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartPreviewComponent } from 'src/app/pages/cart/components/cart-preview/cart-preview.component';
+import { OrderComponent } from 'src/app/pages/cart/components/order/order.component';
 
 const routes: Routes = [
 	{
@@ -11,6 +12,31 @@ const routes: Routes = [
 			breadcrumb: [
 				{
 					label: 'Trang chủ',
+					url: '/',
+				},
+				{
+					label: 'Giỏ hàng',
+					url: '',
+				},
+			],
+		},
+	},
+	{
+		path: 'thanh-toan',
+		component: OrderComponent,
+		data: {
+			title: 'Thanh toán',
+			breadcrumb: [
+				{
+					label: 'Trang chủ',
+					url: '/',
+				},
+				{
+					label: 'Giỏ hàng',
+					url: '/gio-hang',
+				},
+				{
+					label: 'Thanh toán',
 					url: '',
 				},
 			],
--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-layout',
@@ -6,5 +6,8 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
+	@Input() style: any;
+	@Input() class: any;
+
 	ngOnInit(): void {}
 }
